9436781ad74e133f52ab329190bd528d2d269995
hsq5508210 <2468624503@qq.com>
2019-05-23 19:46:04 +0800

    fengzhuang

--- IO.py
+++ IO.py
@@ -1,1 +1,12 @@
-import DPT 
+import DPT
+import csv
+
+class IO(object):
+    def __init__():
+    
+    def readData(path, fileType = csv):
+        if fileType == "csv":
+            csvfile = np.loadtxt(path,  dtype=np.str, delimiter=",")
+            data = csvfile
+            return data
+        #else:
