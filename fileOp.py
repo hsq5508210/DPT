05e76ce13e7b57fb3cae37cde7244e52f5e53e80
hsq5508210 <2468624503@qq.com>
2019-06-03 16:01:25 +0800

    1

---
+++ fileOp.py
@@ -0,0 +1,9 @@
+import IO
+import os 
+
+def file_name(path):
+    file = []
+    for files in os.walk(path):
+        file.append(files)
+    return file
+
--- test2.py
+++
@@ -1,6 +0,0 @@
-import IO
-
-path = "/data2/lt/ctr/content_info/content_info.csv"
-data = readData(path, fileType = 'csv')
-print(data.shape)
-writeData("/data2/lt/ctr/train/", data, "content_info", fileType = 'npy')
