7e822c9189f7efe9782f71fa01b978b12b0ed7ab
hsq5508210 <2468624503@qq.com>
2019-06-03 15:44:17 +0800

    1

--- test2.py
+++ test2.py
@@ -1,3 +1,6 @@
 import IO
 
-read
+path = "/data2/lt/ctr/ad_info/ad_info.csv"
+data = readData(path, fileType = 'csv')
+print(data.shape)
+writeData("/data2/lt/ctr/train/", data, "ad_info", fileType = 'npy')
