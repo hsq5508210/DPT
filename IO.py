8e0b6029b72b0d0f27d7c43e5290f01283075ee8
hsq5508210 <2468624503@qq.com>
2019-06-03 12:19:05 +0800

    1

--- IO.py
+++ IO.py
@@ -2,7 +2,10 @@ import DPT
 import csv 
 import numpy as np 
 import pandas as pd 
+import time 
 def readData(path, fileType = 'csv'):
+    print("reading...")
+    t_s = time.time()
     if fileType == "csv":
         csvfile = pd.read_csv(path)
         data = csvfile
@@ -11,12 +14,17 @@ def readData(path, fileType = 'csv'):
     if fileType == "npy":
         data = np.load(path)
     print("Done")
+    t_e = str(time.time() - t_s) 
+    print("spend " + t_e +'s')
     return data
     #save the npmat type data 
 def writeData(path, data, fileName, fileType = 'bin'):
+    t_s = time.time()
     path = path+fileName+'.'+fileType 
     if fileType == 'bin':
         data.tofile(path)
     if fileType == "npy":
         np.save(path, data)
     print("Done")
+    t_e = str(time.time() - t_s)
+    print("spend " + t_e +'s')
--- test.py
+++ test.py
@@ -1,10 +1,7 @@
 import IO 
 
-path = "/Users/sqh/Projects/DataProcessTool/data.csv"
+path = "/Users/sqh/Projects/2019_520_compete/5_20_data/trainset/train_20190518.csv"
 data = IO.readData(path)
 print(data)
 print(data.shape)
-IO.writeData("/Users/sqh/Projects/DataProcessTool/", data, "data", "npy")
-data = IO.readData("/Users/sqh/Projects/DataProcessTool/data.npy", "npy")
-print(data.shape)
-print(data)
+IO.writeData("/Users/sqh/Projects/2019_520_compete/5_20_data", data, "trainSet", "npy")
