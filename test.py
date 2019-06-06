dbe285a69541aee271f0d69f56625b7bdfce0d2f
hsq5508210 <2468624503@qq.com>
2019-06-06 22:05:38 +0800

    1

--- test.py
+++ test.py
@@ -1,7 +1,24 @@
 import IO 
+import fileOp
 
-path = "/Users/sqh/Projects/2019_520_compete/5_20_data/trainset/train_20190518.csv"
-data = IO.readData(path)
-print(data)
-print(data.shape)
-IO.writeData("/Users/sqh/Projects/2019_520_compete/5_20_data", data, "trainSet", "npy")
+
+path1 = "/Users/sqh/Projects/2019_520_compete/5_20_data/test_data/train_demo.csv"
+path2 = "/Users/sqh/Projects/2019_520_compete/5_20_data/test_data/userInfo_demo.csv"
+path3 = "/Users/sqh/Projects/2019_520_compete/5_20_data/test_data/adInfo_demo.csv"
+path4 = "/Users/sqh/Projects/2019_520_compete/5_20_data/test_data/contentInfo_demo.csv"
+traindata = IO.readData(path1, "csv")
+print(traindata.shape)
+print(traindata)
+userinfo = IO.readData(path2, "csv")
+print(userinfo.shape)
+print(userinfo)
+adinfo = IO.readData(path3, "csv")
+print(adinfo.shape)
+print(adinfo)
+content = IO.readData(path4, "csv")
+print(content.shape)
+print(content)
+
+userId = traindata.iloc[:,1:2]
+print("user id is:\n",userId)
+
