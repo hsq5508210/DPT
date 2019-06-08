362f1c6a6f0beae4deb394e19d45abf932590ef0
hsq5508210 <2468624503@qq.com>
2019-06-08 22:42:53 +0800

    r

--- IO/readData.py
+++
@@ -1,3 +0,0 @@
-import IO
-
-s
---
+++ processData.py
@@ -0,0 +1,12 @@
+import IO 
+import pandas as pd 
+
+#uidPath = "/Users/sqh/Projects/2019_520_compete/5_20_data/test_data/userInfo_demo.csv"
+#trainPath = "/Users/sqh/Projects/2019_520_compete/5_20_data/test_data/userInfo_demo.csv"
+#
+#train = IO.readData(trainPath, 'csv')
+#print("before del:\n", train)
+#train = train[~train.isin(['NULL'])]
+#print("after del:\n", train)
+
+
