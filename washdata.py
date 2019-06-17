16405ec43eb9f27d948c999d9ae920403a3e50cd
hsq5508210 <2468624503@qq.com>
2019-06-17 14:14:46 +0800

    complete the washdata.py(3th)

--- washdata.py
+++ washdata.py
@@ -2,6 +2,7 @@ import IO
 import pandas as pd
 #-------------------------------------
 # read the data
+print("Reading all the data.....")
 path1 = "/data2/lt/ctr/train/trainSet.npy"
 path2 = "/data2/lt/ctr/train/user_info.npy"
 path3 = "/data2/lt/ctr/train/ad_info.npy"
@@ -16,10 +17,24 @@ userId = pd.DataFrame(traindata).iloc[:,1:2]
 #-------------------------------------------
 
 # generate a hash map table from "userId"
+print("Generating the userId-hash..... ")
 userId["hash"] = 0
 print("Generate userId-hash complete!")
 
+# Transform format of the all data.
+print("Transforming the data.....")
+train_pd = (pd.DataFrame(traindata)).reindex(columns = ['label', 'uId', 'adId', 'operTime', 'siteId', 'slotId', 'contentId', 'netType'])
+del traindata
+userinfo_pd = (pd.DataFrame(userinfo)).reindex(columns = ['uId', 'age', 'gender', 'city', 'province', 'phoneType', 'carrier'])
+del userinfo
+content_pd = (pd.DataFrame(content)).reindex(columns = ['contentId', 'firstClass', 'secondClass'])
+del content
+adinfo_pd = (pd.DataFrame(adinfo)).reindex(colunms = ['adId', 'billId', 'primId', 'creativeType', 'interType', 'spreadAppId'])
+del adinfo
+print("Transformed all the data!")
 
+# Index the "NULL" data and mark the hash map.
+print(userinfo_pd.columns)
 
 # Clean the memory.
 exit()
