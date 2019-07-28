2abaa4fe3a94e0e554184f8ff4fe74024f38b821
hsq5508210 <2468624503@qq.com>
2019-07-28 20:46:16 +0800

    Complete the 'BP' in 'NNmodels.py'.

--- NNmodels.py
+++ NNmodels.py
@@ -1,17 +1,20 @@
 from numba import cuda, jit, int64, int32, float32
 import numpy as np
 from sympy import *
+
 class _NN:
 
-    def __init__(self, layer, input, label, activate):
+    def __init__(self, layer, input, label, activate, learningRate):
         """
         list :param layer: list of neuron's number of each layer.
         str :param activate: type of the activate function.
         """
+        self.learningRate = learningRate
         self.layer = self.getLayers(layer)
         self.activate = activate
         self.inputData = input
         self.label = label
+        self.loss = 0
         self.output = 0
     def getLayers(self, layer):
         """
@@ -21,7 +24,7 @@ class _NN:
         L = []
         m = len(layer) - 1
         for i in range(m):
-            L.append(_layer(layer[i], layer[i + 1]))
+            L.append(_layer(layer[i], layer[i + 1], i))
         return L
 
     def getInput(self):
@@ -40,6 +43,9 @@ class _NN:
         m = len(everyLayers)
         input = self.getInput()
         for layer in everyLayers:
+            # print("layer index is: ", layer.layerIdx, ", out shape is: ",
+            #       layer.outNodesNum, ", W shape is :",
+            #       layer.W_mat.shape, ' ', layer.bias.shape)
             W.append(layer.W_mat)
             B.append(layer.bias)
         x = 0
@@ -49,6 +55,7 @@ class _NN:
             if i == 0:
                 x = input
             x = self.act(self.runLayer(w, x) + bias)
+            self.layer[i].outputVal = x
         return x
 
     def runLayer(self, W, x):
@@ -65,37 +72,115 @@ class _NN:
                 x[i] = sigmoid(x[i]).val
         return x
 
-    def getGrad(self, index, delta):
+    # def getDiff(self, x, fun):
+    #     """
+    #
+    #     :param x: value of x.
+    #     :param fun: function form.
+    #     :return: diff value.
+    #     """
+    #     x, y = symbols('x, y')
+    #     res =
+
+    def getGrad(self, func, x_name, x_val):
+        """
+
+        :param func:
+        :param x_name:
+        :param x_val:
+        :return:
+        """
+        return diff(func, x_name)
+
+    def updateWeight(self, layerIdx, grad_B, grad_W):
+        """"""
+        learningRate = self.learningRate
+        self.layer[layerIdx].W_mat = self.layer[layerIdx].W_mat - \
+                                     (learningRate * grad_W)
+        self.layer[layerIdx].bias = self.layer[layerIdx].bias - \
+                                    (learningRate * grad_B)
+
+    def getDelta(self, layerIndex, outputLayerMark):
         """"""
 
-    def updateWeight(self):
-        """"""
-
+    @jit
     def backPropaganda(self):
+        """
+        execute the back propaganda procedure one time.
+        """
         output = self.feedForward(inputData=self.inputData)
-        loss = 0.5 * ((self.label - output) ** 2)
+        lossGrad = output - self.label
+        self.loss = (0.5 * (lossGrad**2)).mean()
+        everyLayers = self.layer
         delta = []
+        outputs = []
+        for i in range(len(everyLayers) - 1, -1, -1):
+            output = everyLayers[i].outputVal
+            theLayer = everyLayers[i]
 
+            nextLayer = everyLayers[i - 1]
+            if i == len(everyLayers) - 1:
+                delta.append(lossGrad * output * (1 - output))
+                continue
+            else:
+                lastLayer = everyLayers[i + 1]
+                delta.append(np.dot(lastLayer.W_mat.T, delta[-1]) * output * (1 - output))
+            grad_B = delta[-1]
+            grad_W = delta[-1] * theLayer.outputVal
+            self.updateWeight(i, grad_B, grad_W)
     def train(self, learningRate):
         """"""
 
 class _layer:
-    def __init__(self, inputNum, outputNum):
+    def __init__(self, inputNum, outputNum, layerIdx):
         """
-        :param inputNum: neuron number of input layer.
-        :param outputNum: neuron number of output layer.
+
+        int :param inputNum: neuron number of input layer.
+        int :param outputNum: neuron number of output layer.
+        int :param layerIdx: index of this layer.
         """
         self.outNodesNum = outputNum
-        self.inNodesNum =  inputNum
+        self.inNodesNum = inputNum
+        self.layerIdx = layerIdx
+        self.outputVal = 0
         self.W_mat = np.random.rand(self.outNodesNum, self.inNodesNum)
         self.bias = np.random.rand(self.outNodesNum, 1)
-    def creatLayer(self):
+        self.grad = []
+
+    # def creatLayer(self):
+    #     """
+    #     generate the layer.
+    #     :return: layer.
+    #     """
+    #     layer = None
+    #     return layer
+
+    def getGrad(self, nodeVal, delta):
+        """"""
+        grad = 0
+        # grad =
+        delta *= grad
+        return grad, delta
+    def updateWeights(self):
+        """"""
+
+class node:
+    def __init__(self, nodeIndex, output, allocateW):
         """
-        generate the layer.
-        :return: layer.
+
+        :param nodeIndex: (i, j, k) 'i' is index of W[], 'j', 'k' is element index of mat W[i].
+        :param output: the node output value.
+        :param allocateW: W[i][j, k] value.
         """
-        layer = 0
-        return layer
+        self.nodeIndex = nodeIndex
+        self.output = output
+        # self.input = None
+        self.allocateW = allocateW
+    def updateW(self, newVal):
+        """"""
+        self.allocateW = newVal
+
+
 class inputLayer:
     """
 
@@ -186,7 +271,18 @@ class matmul(mul):
     def mul_G(self):
         """"""
 
-x = np.random.rand(2,1)
-a = _NN([2, 4, 5, 4, 2], x, "sigmoid")
-print(a.feedForward(x))
+x = np.array([[1],
+              [2],
+              [1]])
+y = np.array([[1],
+              [0]])
+# for i in range(100):
+#     x.append(np.random.rand(2,1))
+#     y.append(np.random.randint(0, 1, (2, 1)))
+a = _NN([3, 4, 5, 4, 2], x, label=y, activate="sigmoid", learningRate=0.08)
+for i in range(100000):
+    if i%100 == 0:
+        print(a.loss)
+    a.backPropaganda()
+
 
